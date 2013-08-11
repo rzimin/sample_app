fc19f1c79c82a3f7c824d3cd043cebd065d26924
Jet Pilot <rzimin@gmail.com>
2013-08-11 12:07:53 -0400

    Finish sign in

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -16,6 +16,7 @@ describe User do
   	it { should respond_to(:password_digest) }
   	it { should respond_to(:password) }
   	it { should respond_to(:password_confirmation) }
+  	it { should respond_to(:remember_token) }
   	it { should respond_to(:authenticate) }
 
   	it { should be_valid }
@@ -33,7 +34,12 @@ describe User do
   	describe "when name is too long" do
   		before { @user.name = "a" * 51 }
   		it { should_not be_valid }
-end
+	end
+
+	describe "remember token" do
+		before { @user.save }
+		its(:remember_token) { should_not be_blank }
+	end
 
 describe "when email format is invalid" do
     it "should be invalid" do
