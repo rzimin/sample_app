b6bcb469b6d1fab75e6e2abae32168dc09fcc652
Jet Pilot <rzimin@gmail.com>
2013-08-15 11:54:09 -0400

    Add user microposts

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -16,7 +16,24 @@ describe "Static pages" do
 
     it_should_behave_like "all static pages"
     it {should_not have_title('| Home') }
-   end
+
+
+    describe "for signed-in users" do
+      let(:user) { FactoryGirl.create(:user) }
+      before do
+        FactoryGirl.create(:micropost, user: user, content: "Lorem ipsum")
+        FactoryGirl.create(:micropost, user: user, content: "Dolor sit amet")
+        sign_in user
+        visit root_path
+      end
+      
+      it "should render the user's feed" do
+        user.feed.each do |item|
+          expect(page).to have_selector("li##{item.id}", text: item.content)
+        end
+      end
+    end
+  end
 
   describe "Help page" do
     before { visit help_path }
@@ -42,6 +59,8 @@ describe "Static pages" do
     it_should_behave_like "all static pages"
   end
 
+
+
   it "should have the right links on the layout" do
     visit root_path
     click_link "About"
@@ -56,5 +75,4 @@ describe "Static pages" do
     click_link "sample app"
     expect(page).to have_title(full_title(''))
   end
-
 end
